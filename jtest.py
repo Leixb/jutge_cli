92e4f7a3c83e5098ec144f1003a28498da02ec44
Leix_b <abone9999@gmail.com>
2017-09-17 23:16:30 +0200

    Implemented jtest

--- jtest.py
+++ jtest.py
@@ -1,9 +1,59 @@
 #!/usr/bin/python3
 
 import logging
-
 log = logging.getLogger('jutge.jtest')
+
+from glob import glob
+from os.path import expanduser, basename
+from subprocess import Popen, PIPE, check_output,CalledProcessError
+
+from tempfile import NamedTemporaryFile
+
+import jgetcode
+import jdownload
+
+class ansi:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    ENDC = '\033[0m'
 
 class jtest:
     def __init__(self,args):
-        pass
+        cont,cor = 0,0
+
+        code = jgetcode.jgetcode(args).code
+        jdownload.jdownload(args)
+
+        for sample_inp in sorted(glob('{}/{}/*.{}'.format(expanduser(args.database),code,args.inp_suffix))):
+            sample_cor = ''.join(sample_inp.split('.')[:-1]) + '.' + args.cor_suffix
+
+            test_input = open(sample_inp,'r')
+            test_output = NamedTemporaryFile()
+
+            p = Popen(args.prog.name, stdin=test_input,stdout=test_output,stderr=PIPE)
+            return_code = p.wait()
+
+            if return_code: log.warning("Program returned {}".format(return_code))
+
+            test_input.seek(0)
+            print(ansi.OKBLUE, ansi.BOLD, '*** Input {}'.format(cont), ansi.ENDC, ansi.HEADER)
+            print(test_input.read(), ansi.ENDC)
+            test_input.close()
+
+            try:
+                out  = check_output([args.diff_prog]+args.diff_flags.split(',')+[test_output.name,sample_cor])
+                print(ansi.OKGREEN, ansi.BOLD, '*** The results match :)', ansi.ENDC, ansi.ENDC)
+                print(out.decode('UTF-8'))
+
+                cor+=1
+
+            except CalledProcessError as err:   # Thrown if files doesn't match
+                print(ansi.FAIL, ansi.BOLD, '*** The results do NOT match :(', ansi.ENDC, ansi.ENDC)
+                print(err.output.decode('UTF-8'))
+
+            test_output.close()
