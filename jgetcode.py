5c73f7b97384c7bcf007a7f4922d09a77a8f95e4
Leix_b <abone9999@gmail.com>
2017-09-17 23:15:05 +0200

    Fixed bug when infereing code from filename

--- jgetcode.py
+++ jgetcode.py
@@ -8,11 +8,14 @@ from os.path import basename
 
 class jgetcode:
     def __init__(self,args):
-        if args.code != '':
+        if args.code != None:
             self.code = args.code
+            log.debug('code in args')
+            log.debug(args.code)
             return
         try:
-            self.code = re.search("({})".format(args.regex),basename(args.prog)).group(1)
+            self.code = re.search("({})".format(args.regex),basename(args.prog.name)).group(1)
+            log.debug(self.code)
         except AttributeError:
             log.error("Code not found, regex failed")
             exit(26)
