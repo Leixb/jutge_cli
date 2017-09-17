f2164a2f1914218b051437259f845239099e77b0
Leix_b <abone9999@gmail.com>
2017-09-18 00:01:05 +0200

    Implemented jarchive

--- jarchive.py
+++ jarchive.py
@@ -4,6 +4,13 @@ import logging
 
 log = logging.getLogger('jutge.jarchive')
 
+from os.path import isdir,expanduser,isfile
+from shutil import move
+
 class jarchive:
     def __init__(self,args):
-        pass
+        title = jprint.jprint(args).title
+        dest = '{}/{}'.format(expanduser(args.folder),title)
+
+        if not isfile(dest) or args.overwrite: move(args.prog,dest)
+
