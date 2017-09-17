b60e752c5e80fca85c841152e531e58a28c316c8
Leix_b <abone9999@gmail.com>
2017-09-17 21:53:50 +0200

    Implemented jadd_cases

--- jadd_cases.py
+++ jadd_cases.py
@@ -3,28 +3,39 @@
 import logging
 log = logging.getLogger('jutge.jadd_cases')
 
-from os.path import isdir
-from os import mkdir
+from os.path import isdir, expanduser,basename
+from os import mkdir,remove
 from glob import glob
+import sys
+
+import re
+
+import jgetcode
 
 class jadd_cases:
     def __init__(self,args):
+
+        code = jgetcode.jgetcode(args).code
+        dest_folder = expanduser('{}/{}'.format(args.database,code))
+
+        if args.delete:
+            for custom_test in glob('{}/custom-*'.format(dest_folder)):
+                remove(custom_test)
+            return
+
         if args.input_file == sys.stdin: print('Enter input:')
         src_inp = args.input_file.read()
         if args.output_file == sys.stdin: print('Enter output:')
         src_cor = args.output_file.read()
 
-        code = getcode(args)
-
-        dest_folder = '{}/{}'.format(args.database,code)
         if not isdir(dest_folder): mkdir(dest_folder)
 
-        files = glob('{}/custom-*',dest_folder)
-        if files: n = basename(files).split('-','.')[-1]+1
+        files = sorted(glob('{}/custom-*'.format(dest_folder)))
+        if files: n = int(re.search('-([0-9]*).',basename(files[-1])).group(1))+1
         else: n = 0
 
         dest = '{}/custom-{}'.format(dest_folder,n)
 
-        open('{}.{}'.format(dest,args.inp_suffix)).write(src_inp)
-        open('{}.{}'.format(dest,args.cor_suffix)).write(src_cor)
+        open('{}.{}'.format(dest,args.inp_suffix),'a').write(src_inp)
+        open('{}.{}'.format(dest,args.cor_suffix),'a').write(src_cor)
 
